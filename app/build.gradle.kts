dc719e782ecce9ef22e3bce229a8cad4b3d7d5bb
Rishav <oneofrishav06@gmail.com>
2026-07-14 17:15:34 +0530

    build: upgrade serialization plugin and fix CI
    Update kotlin-serialization plugin to 2.2.10 and ensure gradlew is executable in the CI pipeline to prevent build failures.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
   alias(libs.plugins.roborazzi)
   alias(libs.plugins.secrets)
   alias(libs.plugins.google.services)
-  id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
+  id("org.jetbrains.kotlin.plugin.serialization") version "2.2.10"
 }
 
 android {
